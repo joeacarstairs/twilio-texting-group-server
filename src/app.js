2b8fea0fd0b514b5ca4349185bf4dfb4455d5875
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2024-03-07 20:47:02 +0000

    Configures stuff

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const { MessagingResponse } = require('twilio').twiml;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.post('/sms', (req, res) => {
+  const 
   const twiml = new MessagingResponse();
   res.type('text/xml').send(twiml.toString());
 });
